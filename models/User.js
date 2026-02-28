0ad09f8e46db493abc9c6cfa4bf736770bcf3a91
Pankajkumar34 <pankajgithubnew@gmail.com>
2026-02-28 20:46:05 +0530

    fix issue

--- models/User.js
+++ models/User.js
@@ -14,7 +14,6 @@ const userSchema = new mongoose.Schema(
     },
     password: {
         type: String,
-        required: true
     },
     name: { type: String, required: true },
     fatherName: { type: String},
